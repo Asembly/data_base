71a374648c85275fe9e00bb03f0052982cbd4d57
nikist-gif <nikitka7770000@mail.ru>
2021-09-06 23:47:49 +0300

    da

--- DataBase/src/data.cpp
+++ DataBase/src/data.cpp
@@ -1,67 +1,112 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-int main()
+class DataBase {
+private:
+	ifstream inputBase_ = {};
+	ofstream outputBase_ = {};
+
+	const string PATH_name = "dataTeacher";
+	const string PATH_number = "numbersTeacher";
+	string password_;
+	string names_;
+	string numbers_;
+
+	int numberTeacher_ = 0;
+	int dataout_ = 0;
+
+	int checkPassword();
+	void setDate();
+public:
+	void dataout();
+	void getDateName();
+	void getDateNumber();
+	void select();
+	DataBase() {
+	    cout << "База данных 9 Б" << endl;
+		checkPassword();
+	}
+};
+
+void DataBase::getDateName() {
+	inputBase_.open(PATH_name);
+	if (!inputBase_.is_open()) {
+		exit(1);
+	}
+	else {
+		}
+	}
+
+void DataBase::getDateNumber() {
+	inputBase_.open(PATH_number);
+	if (!inputBase_.is_open()) {
+		exit(2);
+	}
+	else {
+	}
+}
+
+void DataBase::setDate() {
+	
+}
+
+int DataBase::checkPassword() {
+	cout << "Введите пароль >> ";
+	cin >> password_;
+
+    if(password_ == "Mirror")
+		cout<< "Доступ разрешён" <<endl;
+	else {
+		cout << "Доступ закрыт" << endl;
+		exit(1);
+	}
+}
+
+void DataBase::select()
+{
+	int indexer_ = 1;
+	getDateName();
+	while (!inputBase_.eof()) 
+	{
+		getline(inputBase_, names_);
+		cout << indexer_++ << " " << names_ << endl;
+	}
+	inputBase_.close();
+	cin >> dataout_;
+}
+
+void DataBase::dataout()
+{
+	int a = 0;
+	int b = 0;
+	getDateName();
+	for (a; a < dataout_; a++)
+	{
+		getline(inputBase_, names_);
+	}
+	cout << endl << "<< " << names_ << " >>" << endl;
+	inputBase_.close();
+	getDateNumber();
+	for (b; b < dataout_; b++)
+	{
+		getline(inputBase_, numbers_);
+	}
+	cout << "<< " << numbers_ << " >>" << "\n\n";
+	inputBase_.close();
+	dataout_ = 0;
+}
+
+int main() 
 {
 	setlocale(LC_ALL, "ru");
-
-	string names[] = { "Арген Артёмовна","Тузова (Математика)","Валентина Олеговна (Географиня)",
-	"Анастасия Сергеевна(Биология)","Елена Юрьевна (Физика)","Татьяна Александровна (Химия)",
-	"Любовь Владимировна (ИЗО)","Владимир Георгиевич (Обществознание)","Артём Петрович (ОБЖ)",
-	"Инесса Николаевна (Англ)","Ольга Викторовна (Англ)","Олеся Сергеевна (Информатика)",
-	"Куликова Татьяна (История)" };
-
-	string numbers[] = { "+7 (908) 178-80-84","+7 (909) 421-35-09"," ? ",
-	"+7 (909) 400-64-81","+7 (928) 192-35-76"," ? ",
-	" ? ","+7 (906) 185-81-15"," ? ",
-	"+7 (903) 474-00-54","+7 (908) 506-76-81"," ? ",
-	" ? " };
-
-	string VK[] = { "https://vk.com/id620982794", "https://vk.com/id41859536", "https://vk.com/id5142366",
-	"https://vk.com/id144990443", " ? ", "https://vk.com/id20288866",
-	"https://vk.com/id332735730", "https://vk.com/id54454443", " ? ",
-	"https://vk.com/id234546389", "https://vk.com/o.kupreeva", "https://vk.com/id32999955",
-	" ? " };
-
-	string password;
-	int who;
-	cout << "Введите пароль >> ";
-	cin >> password;
-	if (password == "Mirror")
-	{
-		cout << "Доступ разрешён" << endl << endl;
-	}
-	else
-	{
-		return 1;
-	}
-	cout << "База данных 8 Б << Upd 2 >>" << endl;
-
+	DataBase dataBase = {};
 	while (true)
 	{
-		for (int a = 0; a <= 12; a++)
-		{
-			cout << a + 1 << " - " << names[a] << endl;
-		}
-		cout << "0 - Выйти\n--------------------------\n> ";
-		cin >> who;
-		if (14 > who > 0)
-		{
-			who -= 1;
-			cout << "\n<< " << names[who] << " >>\n"
-				<< "<< " << numbers[who] << " >>\n"
-				<< "<< " << VK[who] << " >>\n\n";
-			continue;
-		}
-		else if (who == 0)
-		{
-			return 2;
-		}
-		else
-		{
-			continue;
-		}
+		dataBase.select();
+		dataBase.dataout();
 	}
 	return 0;
 }
